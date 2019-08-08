9d75ecdcdfebd1dd421b561f3d0f0877070d1702
Theodore Nguyen <theo@Theodores-MBP.modem>
2019-08-09 07:34:23 +1000

    Updates

---
+++ precache-manifest.89011f8dde2405bd2d568cf1c24d19a3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a446bf312e5d358d1b5bf489b1473684",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "4e2b08b49f7f7fd50f6c",
+    "url": "/first-react-app/static/css/2.9fa8a9d7.chunk.css"
+  },
+  {
+    "revision": "6a11bfc635f8b6d59c93",
+    "url": "/first-react-app/static/css/main.f3049eca.chunk.css"
+  },
+  {
+    "revision": "4e2b08b49f7f7fd50f6c",
+    "url": "/first-react-app/static/js/2.8407db76.chunk.js"
+  },
+  {
+    "revision": "6a11bfc635f8b6d59c93",
+    "url": "/first-react-app/static/js/main.9b362ec2.chunk.js"
+  },
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  },
+  {
+    "revision": "e4f2da9601ffd1056bd7eeed169905b3",
+    "url": "/first-react-app/static/media/background.e4f2da96.jpg"
+  },
+  {
+    "revision": "d515da590f5006457ef7536bec0bc2b8",
+    "url": "/first-react-app/static/media/coding.d515da59.jpg"
+  },
+  {
+    "revision": "1aa7a9f6d81c86c3ab5975b6e06df558",
+    "url": "/first-react-app/static/media/dev.1aa7a9f6.jpg"
+  },
+  {
+    "revision": "732d2e24f8a1c22c3739d4532f449356",
+    "url": "/first-react-app/static/media/header-background.732d2e24.jpg"
+  },
+  {
+    "revision": "5cf63dde57aef8f8bf22ef901564026d",
+    "url": "/first-react-app/static/media/marketing.5cf63dde.jpg"
+  }
+]);
