c06bd5f08bdbb18e15266d9af358253d29883e47
Theodore Nguyen <theodorenguyen45@gmail.com>
2019-11-04 11:34:22 +1000

    make it works for different faces

--- src/Components/FaceRecognition.js
+++ src/Components/FaceRecognition.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from 'react'
 
-export default ({ imageUrl, box }) => {
+export default ({ imageUrl, boxes }) => (
+  <div className='flex justify-center mb4 relative'>
+    <img
+      id='inputimage'
+      src={imageUrl}
+      width='300px'
+      height='auto'
+      alt=''
+    />
+
+    {boxes.map(box => (
+      <Face box={box} />
+    ))}
+  </div>
+)
+
+const Face = ({ box }) => {
   return (
-    <div className='flex justify-center mb4 relative'>
-      <img id='inputimage' src={imageUrl} width='300px' height='auto' alt='' />
-      <div
-        className='bounding-box'
-        style={{
-          top: box.topRow,
-          right: box.rightCol,
-          bottom: box.bottomRow,
-          left: box.leftCol
-        }}
-      ></div>
-    </div>
-  );
-};
+    <div
+      className='bounding-box'
+      style={{
+        top: box.topRow,
+        right: box.rightCol,
+        bottom: box.bottomRow,
+        left: box.leftCol
+      }}
+    ></div>
+  )
+}
--- src/Components/SmartBrain.js
+++ src/Components/SmartBrain.js
@@ -1,41 +1,46 @@
-import React, { Component } from 'react';
-import Rank from './Rank';
-import FaceRecognition from './FaceRecognition';
+import React, { Component } from 'react'
+import Rank from './Rank'
+import FaceRecognition from './FaceRecognition'
 
 export default class SmartBrain extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
-    };
+      box: []
+    }
   }
 
-  calculateFaceLocation = data => {
-    const calrifaiFace = data;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
+  calculateFaceLocation = dataRegions => {
+    const dataReturn = dataRegions.map(data => {
+      const calrifaiFace = data.region_info.bounding_box
 
-    return {
-      leftCol: calrifaiFace.left_col * width,
-      topRow: calrifaiFace.top_row * height,
-      rightCol: width - calrifaiFace.right_col * width,
-      bottomRow: height - calrifaiFace.bottom_row * height
-    };
-  };
+      const image = document.getElementById('inputimage')
+      const width = Number(image.width)
+      const height = Number(image.height)
+
+      return {
+        leftCol: calrifaiFace.left_col * width,
+        topRow: calrifaiFace.top_row * height,
+        rightCol: width - calrifaiFace.right_col * width,
+        bottomRow: height - calrifaiFace.bottom_row * height
+      }
+    })
+
+    return dataReturn
+  }
 
   displayFaceBox = box => {
-    this.setState({ box });
-  };
+    this.setState({ box })
+  }
 
   onInputChange = event => {
-    this.setState({ input: event.target.value });
-  };
+    this.setState({ input: event.target.value })
+  }
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+    this.setState({ imageUrl: this.state.input })
     fetch('https://guarded-spire-67673.herokuapp.com/imageurl', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -48,25 +53,29 @@ export default class SmartBrain extends Component {
         if (response) {
           this.displayFaceBox(
             this.calculateFaceLocation(
-              response.outputs[0].data.regions[0].region_info.bounding_box
+              response.outputs[0].data.regions
             )
-          );
-          fetch('https://guarded-spire-67673.herokuapp.com/image', {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              id: this.props.user.id
-            })
-          })
+          )
+          // console.log(response.outputs[0].data.regions)
+          fetch(
+            'https://guarded-spire-67673.herokuapp.com/image',
+            {
+              method: 'PUT',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                id: this.props.user.id
+              })
+            }
+          )
             .then(res => res.json())
             .then(count => {
-              this.props.updateEntry(count);
+              this.props.updateEntry(count)
             })
-            .catch(err => console.log(err));
+            .catch(err => console.log(err))
         }
       })
-      .catch(err => console.log(err));
-  };
+      .catch(err => console.log(err))
+  }
 
   render() {
     return (
@@ -74,7 +83,10 @@ export default class SmartBrain extends Component {
         <div id='SmartBrain' className='tc mv5 w-80-l'>
           <h1 className='ttu'>Smart Brain</h1>
           <h3 className='i fw1'>This is a face recognition</h3>
-          <Rank name={this.props.user.name} entries={this.props.user.entries} />
+          <Rank
+            name={this.props.user.name}
+            entries={this.props.user.entries}
+          />
           <div className='custom-grid'>
             <input
               className='customInput f4 pa3 ba b--green bg-lightest-blue'
@@ -90,8 +102,12 @@ export default class SmartBrain extends Component {
             </button>
           </div>
         </div>
-        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+
+        <FaceRecognition
+          imageUrl={this.state.imageUrl}
+          boxes={this.state.box}
+        />
       </div>
-    );
+    )
   }
 }
